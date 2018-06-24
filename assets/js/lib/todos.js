33c9300460ba4fed3b74dfaa6fe0be5016ec1427
MReinhart1 <michaelreinhart112@gmail.com>
2018-06-24 01:31:27 -0400

    Complete todo list for a website

---
+++ assets/js/lib/todos.js
@@ -0,0 +1,36 @@
+
+
+
+
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("compleated");
+});
+
+
+$("ul").on("click","span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+
+
+});
+
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+       var todoText = $(this).val();
+       $("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</li>");
+       $("input").val('');
+    }
+});
+
+
+$("button").click(function(){
+    $("li").add("test");
+});
+
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+})
